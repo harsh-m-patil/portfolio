617570a2ddbc27e6e2fb5f14d1a85d346b37a1fb
Harshwardhan <135024692+harsh-m-patil@users.noreply.github.com>
2024-12-19 11:19:48 +0530

    add skills

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,13 +1,16 @@
 "use client";
 
+import { Orbits } from "./Orbits";
+
 export default function Hero() {
   return (
-    <div className="h-screen rounded-md flex flex-col items-center justify-center relative w-full">
-      <h2 className="relative flex-col md:flex-row z-10 text-3xl md:text-5xl md:leading-tight max-w-5xl mx-auto text-center tracking-tight font-medium bg-clip-text text-transparent bg-gradient-to-b from-neutral-800 via-white to-white flex items-center gap-2 md:gap-8">
-        <span> Harshwardhan</span>
-        <span className="text-white text-lg font-thin">x</span>
-        <span>Full Stack Web Developer</span>
-      </h2>
+    <div className="grid grid-cols-3 h-screen">
+      <div className="flex flex-col col-span-1 bg-transparent items-center justify-center relative">
+        <h2 className="relative flex-col md:flex-row z-10 text-3xl md:text-5xl md:leading-tight max-w-5xl mx-auto text-center tracking-tight font-medium bg-clip-text text-transparent bg-gradient-to-b from-neutral-800 via-white to-white flex items-center gap-2 md:gap-8">
+          <span> Harshwardhan</span>
+        </h2>
+      </div>
+      <Orbits />
     </div>
   );
 }
